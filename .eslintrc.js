c6cbe645d6fe1f192ae6a323eeda6af734514242
horizon <91zyutz@gmail.com>
2019-02-23 19:40:04 +0900

    [fix]eslintのrules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-     "vue/html-self-closing": 0,
+    "vue/html-self-closing": 0,
+    "vue/max-attributes-per-line": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
